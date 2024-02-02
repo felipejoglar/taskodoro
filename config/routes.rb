993453d603da1ab8c9af69004aec9425e708a0a0
Felipe Joglar <felipejoglar@gmail.com>
2024-02-02 09:31:28 +0100

    Remove unneeded `landing` route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,18 @@
+#    Copyright 2024 Felipe Joglar
+#
+#    Licensed under the Apache License, Version 2.0 (the "License");
+#    you may not use this file except in compliance with the License.
+#    You may obtain a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS,
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    See the License for the specific language governing permissions and
+#    limitations under the License.
+
 Rails.application.routes.draw do
-  get 'landing/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -8,4 +21,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root "landing#index"
+
+
 end
