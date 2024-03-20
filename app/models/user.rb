8aa4f2299be518d916b82f58d12e9b9abf5275ce
Felipe Joglar <felipejoglar@gmail.com>
2024-03-20 11:25:44 +0100

    Extract `Resettable` concern from `User` model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  include Resettable
+
   has_secure_password
 
   has_many :projects, dependent: :destroy
@@ -12,18 +14,4 @@ class User < ApplicationRecord
   validates :password, format: PASSWORD_REQUIREMENTS
 
   normalizes :email, with: -> (email) { email.strip.downcase }
-
-  def password_reset_requested
-    UserMailer.with(user: self, token: generate_token_for(:password_reset))
-              .password_reset
-              .deliver_later
-  end
-
-  private
-
-  RESET_PASSWORD_TOKEN_EXPIRATION = 15.minutes
-
-  generates_token_for :password_reset, expires_in: RESET_PASSWORD_TOKEN_EXPIRATION do
-    password_salt&.last(12)
-  end
 end
