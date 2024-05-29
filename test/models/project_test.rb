a5e59e8bfbf440e14fbb038e003560e36d97b1a8
Felipe Joglar <felipejoglar@gmail.com>
2024-05-29 16:17:20 +0200

    Add missing project model tests

--- test/models/project_test.rb
+++ test/models/project_test.rb
@@ -4,8 +4,45 @@ class ProjectTest < ActiveSupport::TestCase
 
   test "project attributes must not be empty" do
     project = Project.new
+
     assert project.invalid?, message: "Project without params should be invalid"
     assert project.errors[:title].any?, message: "Expected name to be present"
     assert project.errors[:user_id].any?, message: "Expected project to belong to an user"
+  end
+
+  test "project's empty description is saved as nil" do
+    project = Project.new(description: "")
+
+    assert_nil project.description
+  end
+
+  test "project's tasks are queried ordered by due_date" do
+    first_task = task_with_due_date(tasks(:one), Date.today)
+    last_task = task_with_due_date(tasks(:two), Date.today.next_day(10))
+
+    tasks = projects(:inbox).tasks
+
+    assert_equal first_task, tasks.first
+    assert_equal last_task, tasks.last
+  end
+
+  test "destroying a project destroys its tasks" do
+    Current.user = users(:felipe)
+    project = users(:felipe).projects.create(title: "A Project")
+    task = project.tasks.create(title: "A Task")
+
+    project.destroy!
+
+    refute Project.find_by(id: project.id)
+    refute Task.find_by(id: task.id)
+  end
+
+  private
+
+  def task_with_due_date(task, due_date)
+    task.due_date = due_date
+    task.save
+
+    task
   end
 end
